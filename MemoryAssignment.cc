ae0b6852ca50e922b8b32dd0213df2af88d6532a
aabagdi <70984836+aabagdi@users.noreply.github.com>
2022-03-19 12:59:21 -0700

    Add files via upload

--- MemoryAssignment.cc
+++ MemoryAssignment.cc
@@ -1,32 +1,33 @@
 #include "MemoryAssignment.hh"
 
 vector<uint16_t>* genBuffer() {
-    vector<uint16_t>* buffer = new vector<uint16_t>(256);
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_int_distribution<> distr(0, 255);
+    vector<uint16_t>* buffer = new vector<uint16_t>(256);                       // initialise vector pointer with size 256
+    random_device rd;                                                           // initialise random device
+    mt19937 gen(rd());                                                          // make a generator using that random device
+    uniform_int_distribution<> distr(0, 255);                                   // make distribution between 0 and 255 (indices of vector)
     for (uint16_t i = 0; i < 256; i++) {
-        (*buffer)[i] = distr(gen);
+        (*buffer)[i] = distr(gen);                                              // pick random number from distribution and store into array
     }
-    shuffle(buffer->begin(), buffer->end(), gen);
+    shuffle(buffer->begin(), buffer->end(), gen);                               // shuffle array to mess with the pre-fetcher
     return buffer;
 }
 
 double timeAccess(vector<uint16_t>* buff, uint64_t buffSize) {
-    struct timespec start, stop;
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_int_distribution<> distr(0, 255);
-    auto index = distr(gen);
-    auto nextIndex = (*buff)[index];
-    clock_gettime(CLOCK_MONOTONIC, &start);
-    for (uint64_t i = 0; i <= buffSize; i++) {
-        cout << nextIndex << endl;
-        nextIndex = (*buff)[nextIndex];
+    struct timespec start, stop;                                                // initialise clocks
+    random_device rd;                                                           // initialise random device
+    mt19937 gen(rd());                                                          // make a generator using that random device
+    uniform_int_distribution<> distr(0, 255);                                   // make distribution between 0 and 255 (indices of vector)
+    auto index = distr(gen);                                                    // pick random number from distribution and use that as vector index
+    auto nextIndex = (*buff)[index];                                            // fetch element from array using that index
+    clock_gettime(CLOCK_MONOTONIC, &start);                                     // begin timing
+    for (uint64_t i = 0; i <= buffSize; i++) {                                  // iterate loop for buffer size
+        cout << nextIndex << endl;                                              // output fetched element
+        nextIndex = (*buff)[nextIndex];                                         // use that fetched element as next index
     }    
-    clock_gettime(CLOCK_MONOTONIC, &stop);
-    double time_taken = ((double)stop.tv_sec + 1.0e-9*stop.tv_nsec) - ((double)start.tv_sec + 1.0e-9*start.tv_nsec);
-    double avgTime = ((time_taken/(buffSize)) * 1e9) / 2;
+    clock_gettime(CLOCK_MONOTONIC, &stop);                                      // end timing
+    double time_taken = ((double)stop.tv_sec + 1.0e-9*stop.tv_nsec)
+     - ((double)start.tv_sec + 1.0e-9*start.tv_nsec);                           // calculate time in seconds
+    double avgTime = ((time_taken/(buffSize)) * 1e9) / 2;                       // convert to ns, divided by 2 as there are two reads in the loop
     return avgTime;
 }
 
@@ -35,9 +36,9 @@ int main(int argc, char* argv[]) {
         cout << "Please enter only one numerical argument." << endl;
         return -1;
     }
-    uint64_t buffSize = atoi(argv[1]);
-    auto randBuff = genBuffer();
-    auto timeTaken = timeAccess(randBuff, buffSize);
-    cout << "Average time per buffer read = " << timeTaken << " ns" << endl;
+    uint64_t buffSize = atoi(argv[1]);                                          // take in buffer size as command-line argument
+    auto randBuff = genBuffer();                                                // generate random array
+    auto timeTaken = timeAccess(randBuff, buffSize);                            // read array and time
+    cout << "Average time per buffer read = " << timeTaken << " ns" << endl;    // print average time per read
     return 0;
 }
--- MemoryAssignment.hh
+++ MemoryAssignment.hh
@@ -8,6 +8,6 @@
 
 using namespace std;
 
-vector<uint16_t>* genBuffer();
+vector<uint16_t>* genBuffer();                                  // function to generate randomised vector of size 256
 
-double timeAccess(vector<uint16_t>* buff, uint64_t buffSize);
+double timeAccess(vector<uint16_t>* buff, uint64_t buffSize);   // function to read array and time how long reads take given buffer size
